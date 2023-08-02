49972a148eaac4bad2aac9e740e3bde3d166c966
Niranjan-KS <niranjan.ks.pro@gmail.com>
2023-08-02 18:02:49 +0530

    commit

---
+++ retrieve_the_value_of_a_text_field/README.md
@@ -0,0 +1,3 @@
+# retrieve_the_value_of_a_text_field
+
+A new Flutter project.
---
+++ retrieve_the_value_of_a_text_field/android/app/src/main/kotlin/com/example/retrieve_the_value_of_a_text_field/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.retrieve_the_value_of_a_text_field
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
